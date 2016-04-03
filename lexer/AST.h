573cf15dd078e54b7900ab16e38085bf342815e1
BryanBonner <bonnerbryan@gmail.com>
2016-04-02 21:41:27 -0700

    Added all constructors of existing classes

--- lexer/AST.h
+++ lexer/AST.h
@@ -83,65 +83,101 @@ private:
 };
 
 class If : public Node {
+public:
+	If(Condition* cond, NodeList* ibody, NodeList* ebody) : condition(cond), ifbody(ibody), elsebody(ebody) {}
 	Condition* condition;
+private:
 	NodeList* ifbody;
 	NodeList* elsebody;
 
 };
 
 class Return : public Node {
+public:
+	Return(Node* express) : expression(express) {}
+private:
 	Node* expression;
 };
 
 class Write : public Node {
+public:
+	Write(Node* express) : expression(express) {}
+private:
 	Node* expression;
 };
 
 class Read : public Node {
+public:
+	Read(NodeList* identifiers) : ids(identifiers) {}
+private:
 	NodeList* ids;
 };
 
 class While : public Node {
+public:
+	While(Condition* cond, NodeList* body) : condition(cond), body(body) {}
+private:
 	Condition* condition;
 	NodeList* body;
 };
 
 class Condition : public Node {
+public:
+	Condition(Token t, Node* l, Node* r) : oper(t), left(l), right(r) {}
+private:
 	Token oper;
 	Node* left;
 	Node* right;
 };
 
 class BinaryExpression : public Node {
+public:
+	BinaryExpression(Token t, Node* l, Node* r) : oper(t), left(l), right(r) {}
+private:
 	Token oper;
 	Node* left;
 	Node* right;
 };
 
 class UrinaryExpression : public Node {
+public:
+	UrinaryExpression(Node* cent) : center(cent) {}
+private:
 	Node* center;
 };
 
 class Identifier : public Node {
+public:
+	Identifier(Token t) : value(t) {}
+private:
 	Token value;
 };
 
 class Integer : public Node {
+public:
+	Integer(Token t) : value(t) {}
+private:
 	Token value;
 };
 
 class Real : public Node {
+public:
+	Real(Token t) : value(t) {}
+private:
 	Token value;
 };
 
 class Bool : public Node {
+public:
+	Bool(Token t) : value(t) {}
+private:
 	Token value;
 };
 
 class FunctionCall : public Node {
+public:
+	FunctionCall(Token t, NodeList* args) : id(t), arguments(args) {}
+private:
 	Token id;
-	NodeList* arguements;
+	NodeList* arguments;
 };
-
-
-
